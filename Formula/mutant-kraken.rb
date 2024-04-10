abdb4f98ed70caec6d4317acc108a14e0447bda3
CircleCI Job <molinajosue92@hotmail.com>
2024-04-10 03:31:19 +0000

    Update mutant-kraken to 0.1.5-beta

--- Formula/mutant-kraken.rb
+++ Formula/mutant-kraken.rb
@@ -4,9 +4,9 @@
 class MutantKraken < Formula
     desc "Mutation Testing tool for Kotlin written in Rust"
     homepage "https://github.com/JosueMolinaMorales/mutant-kraken"
-    url "https://github.com/josuemolinamorales/mutant-kraken/releases/download/v0.1.4-beta/mutant-kraken-macos.tar.gz"
-    sha256 "bbe9b6dfa81f0c80bb2a7d8aa3c6efdd3225c479876a74712865dd2845bc9105"
-    version "0.1.4-beta"
+    url "https://github.com/josuemolinamorales/mutant-kraken/releases/download/v0.1.5-beta/mutant-kraken-macos.tar.gz"
+    sha256 "76707345d08c6fc4f346b0572259e03523eb447920b6721b65268aa4c7247d34"
+    version "0.1.5-beta"
   
     def install
       bin.install "mutant-kraken"
