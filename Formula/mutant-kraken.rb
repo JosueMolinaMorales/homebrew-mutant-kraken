705633b76a0a520f21f55298c11a6af0c1c6f1a5
CircleCI Job <molinajosue92@hotmail.com>
2024-07-01 13:07:27 +0000

    Update mutant-kraken to 0.1.3

--- Formula/mutant-kraken.rb
+++ Formula/mutant-kraken.rb
@@ -4,11 +4,17 @@ class MutantKraken < Formula
   version "0.1.3"
   if OS.mac?
     url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/0.1.3/mutant-kraken-macos.tar.gz"
-    sha256 "90410d5e2395f51cd9f18952e948d36c989e3524d2d52ab99ca577eb86098b2f"
+    sha256 "2ee0d200bac4c560e057427b67c9e4e45c4e9eb7f5f31a21c9ba4cbf95bbc958"
   elsif OS.linux?
     url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/0.1.3/mutant-kraken-linux.tar.gz"
-    sha256 "923505810a45334cbe94a0483d76993872f29d17918bc79c52c1a6d2d3c79863"
+    sha256 "a4f24cc17ad83717a068638dbda0dae20a13b6ebf7313ce42042b2025d66595c"
+  end
+
   def install
     bin.install "mutant-kraken"
+  end
+
+  test do
+    system "#{bin}/mutant-kraken", "--version"
   end
 end
