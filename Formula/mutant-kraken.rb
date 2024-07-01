e30846ad69619e7fc61e2d894d9464007dc36383
CircleCI Job <molinajosue92@hotmail.com>
2024-07-01 15:39:18 +0000

    Update mutant-kraken to 0.1.3

--- Formula/mutant-kraken.rb
+++ Formula/mutant-kraken.rb
@@ -1,13 +1,13 @@
 class MutantKraken < Formula
   desc "Mutation Testing tool for Kotlin written in Rust"
   homepage "https://github.com/JosueMolinaMorales/mutant-kraken"
-  version "0.1.2"
+  version "0.1.3"
   if OS.mac?
-    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.2/mutant-kraken-macos.tar.gz"
-    sha256 "680a9faf35a1cdd5f99a0f856bd6a6b69f0a1943d0ca2241e7a2ecf8f2d7026b"
+    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.3/mutant-kraken-macos.tar.gz"
+    sha256 "1c943121a7e7f37f3d13407909202b2a6093d11ec479bdfc2933a3afde2074f5"
   elsif OS.linux?
-    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.2/mutant-kraken-linux.tar.gz"
-    sha256 "beb9ae3bacc17cc5abeb5bc958f45925382ecea3ea950803f1b35517a0a0e830"
+    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.3/mutant-kraken-linux.tar.gz"
+    sha256 "f62408811f503fba4c2aae30fded1206d63d43767f225f635db25845d614e86a"
   end
 
   def install
