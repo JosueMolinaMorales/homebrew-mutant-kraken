47a7b0b87633129eddac85fcc11ef99165a26481
CircleCI Job <molinajosue92@hotmail.com>
2024-07-09 12:22:49 +0000

    Update mutant-kraken to 0.1.5

--- Formula/mutant-kraken.rb
+++ Formula/mutant-kraken.rb
@@ -3,12 +3,22 @@ class MutantKraken < Formula
   homepage "https://github.com/JosueMolinaMorales/mutant-kraken"
   version "0.1.5"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-arm64.tar.gz"
-    sha256 "5ddb445e3691f31a5d06bfdc622a25a783434531420892d3e1e5f8a93d9c561b"
-  elsif Hardware::CPU.intel?
-    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-x86_64.tar.gz"
-    sha256 "d52f738cfe81b7522a0252cf85f27e183573664d7914fef2d0490d24789141d8"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-aarch64-apple-darwin.tar.gz"
+      sha256 "5678abc7c3fabf41ec22f1dcb440d40f2432015dce99005f62d377590ec5cafc"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-x86_64-apple-darwin.tar.gz"
+      sha256 "3bd0f65553012d6c355a5e76c7b1724471da7485b77e8ab23c405ec5eed36b8c"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "9ed8857dfa8ba5ef4afbb678fce0031321583f5ecd2e54fb3b90891a3731c112"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "9e4f5a4e73936d71ec96435a1f7e4775ea705ff5b9c46637a6427dd1d549a5d7"
+    end 
   end
 
   def install
