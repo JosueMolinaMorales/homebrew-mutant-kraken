78087365fb6a504b1bb8f0d0bb097768bb260b85
Josue Morales <molinajosue92@hotmail.com>
2024-07-08 22:03:32 -0400

    update 0.1.5

--- Formula/mutant-kraken.rb
+++ Formula/mutant-kraken.rb
@@ -2,12 +2,12 @@ class MutantKraken < Formula
   desc "Mutation Testing tool for Kotlin written in Rust"
   homepage "https://github.com/JosueMolinaMorales/mutant-kraken"
   version "0.1.5"
-  if OS.mac?
-    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-macos.tar.gz"
-    sha256 "d20e71ee0d84f43230d3d1bb55f4652180aeacd9d27704a0328a161f501a7a9e"
-  elsif OS.linux?
-    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-linux.tar.gz"
-    sha256 "ae0c7f5fb086b4a360a12e6c6cab836644b7248371cdb4a6f352420d4c586ea5"
+  if Hardware::CPU.arm?
+    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-arm64.tar.gz"
+    sha256 "$ARM_SHA256"
+  elsif Hardware::CPU.intel?
+    url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-x86_64.tar.gz"
+    sha256 "$X86_SHA256"
   end
 
   def install
