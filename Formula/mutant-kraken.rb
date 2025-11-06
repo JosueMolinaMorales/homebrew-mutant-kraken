7033ee35f788bf93a1e41b932ed804430dd29da8
CircleCI Job <molinajosue92@hotmail.com>
2025-11-06 17:12:29 +0000

    Update mutant-kraken to 0.1.6

--- Formula/mutant-kraken.rb
+++ Formula/mutant-kraken.rb
@@ -1,23 +1,23 @@
 class MutantKraken < Formula
   desc "Mutation Testing tool for Kotlin written in Rust"
   homepage "https://github.com/JosueMolinaMorales/mutant-kraken"
-  version "0.1.5"
+  version "0.1.6"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-aarch64-apple-darwin.tar.gz"
-      sha256 "dd58bd94161e3542683843c8e38525a378c0e5992cb39b83d60adf5200825984"
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.6/mutant-kraken-aarch64-apple-darwin.tar.gz"
+      sha256 "b8571c68fce70ee91d38f98f26f71ecfb9b888bb49dbb080412237c752ba6d11"
     elsif Hardware::CPU.intel?
-      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-x86_64-apple-darwin.tar.gz"
-      sha256 "94b37e33dc40ed68c1add2f186456c0e82cb652d7e4c249fbe8ab1e7ab16391e"
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.6/mutant-kraken-x86_64-apple-darwin.tar.gz"
+      sha256 "e10b2cc2e39bf7567f6b236330d3560e740f8570910e0e753bda75cf2f471be1"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e1ec6a22954de9e9460658a71509f99216b68d553806ccedad9c557de260999e"
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.6/mutant-kraken-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "5cfd4f9f1f241f8004acc19385ab78ba9696e1ae49bb638004893d3d12498999"
     elsif Hardware::CPU.intel?
-      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.5/mutant-kraken-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "975b1691e30ec1bd9a7a3102b56ed8fba34d74ef88feb906469b0a7f7166cff2"
+      url "https://github.com/JosueMolinaMorales/mutant-kraken/releases/download/v0.1.6/mutant-kraken-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "68804ee89a9e9bcb53136b751a083e38ff5bd8357925d62b691d53461b179ad5"
     end 
   end
 
